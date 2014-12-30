1d3b10d45328050c8a3aa30ed24dae75c23da099
Niraj Pant <nirajpant7@gmail.com>
2014-12-30 15:20:08 -0600

    init problem 2

---
+++ Problem-2/Problem-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Problem-2
+//
+//  Created by Niraj  on 12/30/14.
+//  Copyright (c) 2014 Niraj Pant. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
