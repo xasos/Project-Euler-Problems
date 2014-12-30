a1f4e7600ce9691fd2209442ca44618cd79ed7b5
Niraj Pant <nirajpant7@gmail.com>
2014-12-30 14:57:17 -0600

    add problem 1

---
+++ Problem-1/Problem-1/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Problem-1
+//
+//  Created by Niraj Pant on 12/30/14.
+//  Copyright (c) 2014 Niraj Pant. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
