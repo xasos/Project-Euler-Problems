c6ac5bdd5e2c06d3bc5bf732b70aac36b70ba8d8
Niraj Pant <nirajpant7@gmail.com>
2015-01-03 15:17:06 -0600

    add prob 3

--- Problem-2/Problem-2/main.cpp
+++ Problem-2/Problem-2/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n1 = 1;
+    int n2 = 2;
+    int sum = 0;
+    int temp = 0;
+    
+    for (int i=0; i<4000000; i++) {
+        temp = n1 + n2;
+        n1 = n2;
+        n2 = temp;
+        if (n2 % 2 == 0) {
+            sum+=n2;
+        }
+    }
+    cout << sum;
+    
+//    cout << "Hello, World!\n";
     return 0;
 }
---
+++ Problem-3/Problem-3/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Problem-3
+//
+//  Created by Niraj  on 1/3/15.
+//  Copyright (c) 2015 Niraj Pant. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
