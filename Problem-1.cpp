d429aec78d0d34cd6ee8fbcb9b51e30a7a99e839
Niraj Pant <nirajpant7@gmail.com>
2015-01-10 13:20:38 -0600

    Clean up

--- Problem-1.cpp
+++ Problem-1.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  Problem-1
-//
-//  Created by Niraj Pant on 12/30/14.
-//  Copyright (c) 2014 Niraj Pant. All rights reserved.
-//
-
 #include <iostream>
 using namespace std;
 
